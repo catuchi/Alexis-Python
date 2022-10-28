d9b7ab8384260bc88b655e05581e199cf6b022e0
Chibu <atuchibueze@gmail.com>
2022-10-28 05:02:38 -0400

    implement google text to speech

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
 import webbrowser
 import time
+import playsound
+import os
+import random
+from gtts import gTTS             # google text to speech
 from time import ctime
 
 r = sr.Recognizer()
@@ -8,38 +12,47 @@ r = sr.Recognizer()
 def record_audio(ask = False):
   with sr.Microphone() as source:
     if ask:
-      print(ask)
+      alexis_speak(ask)
     audio = r.listen(source)
     voice_data = ''
     try:
       voice_data = r.recognize_google(audio)
       # print(voice_data)
     except sr.UnknownValueError:
-      print('Sorry, I did not get that')
+      alexis_speak('Sorry, I did not get that')
     except sr.RequestError:
-      print('Sorry, my speech service is down')
+      alexis_speak('Sorry, my speech service is down')
     return voice_data
+
+def alexis_speak(audio_string):
+  tts = gTTS(text=audio_string, lang='en')
+  r = random.randint(1, 10000000)
+  audio_file = f'audio-{r}.mp3'
+  tts.save(audio_file)
+  playsound.playsound(audio_file)
+  print(audio_string)
+  os.remove(audio_file)
 
 def respond(voice_data):
   if 'what is your name' in voice_data:
-    print('My name is Alexis')
+    alexis_speak('My name is Alexis')
   if 'what time is it' in voice_data:
-    print(ctime())
+    alexis_speak(ctime())
   if 'search' in voice_data:
     search = record_audio('What do you want to search for?')
     url = 'https://google.com/search?q=' + search
     webbrowser.get().open(url)
-    print('Here is what I found for ' + search)
+    alexis_speak('Here is what I found for ' + search)
   if 'find location' in voice_data:
     location = record_audio('What is the location?')
     url = f'https://google.nl/maps/place/{location}/&amp;'
     webbrowser.get().open(url)
-    print(f'Here is the location of {location}')
+    alexis_speak(f'Here is the location of {location}')
   if 'exit' in voice_data:
     exit()
 
 time.sleep(1)
-print('How can I help you?')
+alexis_speak('How can I help you?')
 while 1:
   voice_data = record_audio()
   respond(voice_data)
